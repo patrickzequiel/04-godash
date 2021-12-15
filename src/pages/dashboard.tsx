af4a0fdef8b23e75c523d88bb94bbe6739aca93e
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-12-15 14:36:43 +0100

    users and create added

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,12 @@
 import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
+import { ApexOptions } from "apexcharts";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
-const options = {
+const options: ApexOptions = {
     chart: {
         toolbar: {
             show: false,
@@ -14,6 +15,7 @@ const options = {
             enabled: false,
         },
         foreColor: theme.colors.gray[500],
+        },
         grid: {
             show: false,
         },
@@ -30,16 +32,15 @@ const options = {
             },
             axisTicks: {
                 color: theme.colors.gray[600],
-            }
-        },
+            },
         categories: [
-            '2021-03-18t00:00:00.000Z',
-            '2021-03-19T00:00:00.000Z',
-            '2021-03-20t00:00:00.000Z',
-            '2021-03-21T00:00:00.000Z',
-            '2021-03-22T00:00:00.000Z',
-            '2021-03-23T00:00:00.000Z',
-            '2021-03-24T00:00:00.000Z',
+            "2021-03-18T00:00:00.000Z",
+            "2021-03-19T00:00:00.000Z",
+            "2021-03-20T00:00:00.000Z",
+            "2021-03-21T00:00:00.000Z",
+            "2021-03-22T00:00:00.000Z",
+            "2021-03-23T00:00:00.000Z",
+            "2021-03-24T00:00:00.000Z"
         ]
     },
     fill: {
