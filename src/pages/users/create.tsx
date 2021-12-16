13541732747ae17a4fd4141a906a8e577858e4ea
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-12-16 15:48:04 +0100

    manutenção do create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Divider, Flex, Heading, HStack, Input, SimpleGrid, VStack } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import Link from "next/link";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { Input } from '../../components/Form';
 
 
 export default function CreateUser() {
@@ -16,32 +17,47 @@ export default function CreateUser() {
                     <Heading size="lg" fontWeight="normal">
                         Criar Usuário
                     </Heading>
-                    
+
                     <Divider my="6" borderColor="gray.700" />
 
                     <VStack spacing="8">
-                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]}  w="100%">
-                            <Input name="name" label="Nome Completo" />
-                            <Input name="email" type="email" label="E-mail" />
+                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+                            <Input
+                                name="name"
+                                label="Nome completo"
+                            />
+                            <Input
+                                name="email"
+                                type="email"
+                                label="E-mail"
+                            />
                         </SimpleGrid>
 
                         <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-                            <Input name="password" type="password" label="Senha" />
-                            <Input name="password_confirmation" type="password" label="Confirmação da Senha" />
+                            <Input 
+                                name="password" 
+                                type="password" 
+                                label="Senha" 
+                            />
+                            <Input 
+                                name="password_confirmation" 
+                                type="password" 
+                                label="Confirmação da Senha" 
+                            />
                         </SimpleGrid>
                     </VStack>
 
                     <Flex mt="8" justify="flex-end">
                         <HStack>
                             <Link href="/users" passHref>
-                            <Button colorScheme="whiteAlpha">
-                                Cancelar
-                            </Button>
+                                <Button colorScheme="whiteAlpha">
+                                    Cancelar
+                                </Button>
                             </Link>
                             <Link href="/users" passHref>
-                            <Button colorScheme="pink">
-                                Salvar
-                            </Button>
+                                <Button colorScheme="pink">
+                                    Salvar
+                                </Button>
                             </Link>
                         </HStack>
                     </Flex>
